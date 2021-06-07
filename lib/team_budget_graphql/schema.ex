1759828326a71373379c2840245853a8513743c4
joaoeliandro <eliandrogermano@gmail.com>
2021-06-07 08:44:14 -0300

    Added mutation create user and error payload graphql

--- lib/team_budget_graphql/schema.ex
+++ lib/team_budget_graphql/schema.ex
@@ -1,9 +1,14 @@
 defmodule TeamBudgetGraphql.Schema do
   use Absinthe.Schema
 
+  import AbsintheErrorPayload.Payload
+
   alias TeamBudgetGraphql.Resolvers
 
   import_types(TeamBudgetGraphql.Types)
+  import_types(AbsintheErrorPayload.ValidationMessageTypes)
+
+  payload_object(:user_payload, :user)
 
   query do
     @desc "Get list of all users"
@@ -12,7 +17,12 @@ defmodule TeamBudgetGraphql.Schema do
     end
   end
 
-  #  mutation do
-
-  #  end
+  mutation do
+    @desc "Create a new User"
+    field :create_user, :user_payload do
+      arg(:user, non_null(:user_input))
+      resolve(&Resolvers.UserResolver.create_users/3)
+      middleware(&build_payload/2)
+    end
+  end
 end
