1b66ebcdbc346be8037278b3b53045366c1371bd
joaoeliandro <eliandrogermano@gmail.com>
2021-06-07 07:56:09 -0300

    Added account module to list users

--- lib/team_budget_graphql/resolvers/user_resolver.ex
+++ lib/team_budget_graphql/resolvers/user_resolver.ex
@@ -1,5 +1,7 @@
 defmodule TeamBudgetGraphql.Resolvers.UserResolver do
+  alias TeamBudget.Accounts
+
   def list_users(_parent, _params, _resolutions) do
-    {:ok, %{email: "teste@example.com"}}
+    {:ok, Accounts.list_users()}
   end
 end
