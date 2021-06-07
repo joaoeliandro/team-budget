f88b13ad5a4affcda214226a2701727655c17de6
joaoeliandro <eliandrogermano@gmail.com>
2021-06-07 07:35:12 -0300

    Added first types and user schema graphql

---
+++ lib/team_budget_graphql/types/user.ex
@@ -0,0 +1,11 @@
+defmodule TeamBudgetGraphql.Types.User do
+  use Absinthe.Schema.Notation
+
+  object :user do
+    field :id, :string
+    field :first_name, :string
+    field :last_name, :string
+    field :email, :string
+    field :role, :string
+  end
+end
