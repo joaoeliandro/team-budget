3358be172643dd9bf6e0dcb3f85a0010b398ee37
joaoeliandro <eliandrogermano@gmail.com>
2021-06-07 08:44:30 -0300

    Added create user resolver graphql

--- lib/team_budget_graphql/resolvers/user_resolver.ex
+++ lib/team_budget_graphql/resolvers/user_resolver.ex
@@ -4,4 +4,8 @@ defmodule TeamBudgetGraphql.Resolvers.UserResolver do
   def list_users(_parent, _params, _resolutions) do
     {:ok, Accounts.list_users()}
   end
+
+  def create_users(_parent, %{user: user}, _resolutions) do
+    Accounts.create_user(user)
+  end
 end
